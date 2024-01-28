aa2f341ac765e9a92d1b6a3437645e302f54a3c9
Artyom <itcoding76@gmail.com>
2024-01-28 19:48:06 +0200

    JJ HomeWorks 04 DB JDBC

---
+++ HomeWorksJJ/src/main/java/ru/gb/hw04/model/Course.java
@@ -0,0 +1,71 @@
+package ru.gb.hw04.model;
+
+import java.util.Random;
+
+public class Course {
+
+    private int id;
+    private String title;
+
+    private int duration;
+
+    private static final String[] titles = new String[]{"Математика", "Философия", "САПР", "Основы электротехники", "Черчение", "Иностранный язык", "История", "Экономика"};
+    private final static Random random = new Random();
+
+
+    public Course(int id, String title, int duration) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+    }
+    public Course(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+    public Course() {
+    }
+
+    public static  Course create(){
+        return new Course(titles[random.nextInt(titles.length)], random.nextInt(2, 6));
+    }
+
+    public void updateDuration(){
+        duration = random.nextInt(2, 6);
+    }
+    public void updateTitle(){
+        title = titles[random.nextInt(titles.length)];
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+
+
+    //region Getters & Setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+//endregion
+}
